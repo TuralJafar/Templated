6f592b839cb214cdf070794460b0f8f11079fa4b
Shahvalad <shahvaladayvazov@gmail.com>
2023-05-25 08:35:08 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Templated.Models;
+
+namespace Templated.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
